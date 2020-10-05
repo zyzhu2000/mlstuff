3ee385b0d3a67119da57e216f087d577d195edf0
Geoffrey Zhu <zyzhu2000@gmail.com>
2020-10-05 03:44:39 -0500

    t

--- likelihoodcharts.py
+++ likelihoodcharts.py
@@ -123,7 +123,7 @@ def make_mimic_keep():
     runs = 5
     suite = TestSuite(0)
     fn = CSequence()
-    runner = MIMICRunner(fn, dict(keep_pct=0.2, pop_size=2000, max_attempts=50))
+    runner = MIMICRunner(fn, dict(keep_pct=0.2, pop_size=1000, max_attempts=50))
     
     curves = make_curve(suite, runner, dict(keep_pct=[0.05, 0.1, 0.2, 0.3, 0.6]), runs=runs, is_product=False)
     plt.figure()
@@ -152,7 +152,7 @@ def make_mimic_pop():
     runs = 5
     suite = TestSuite(0)
     fn = CSequence()
-    runner = MIMICRunner(fn, dict(keep_pct=0.2, pop_size=2000, max_attempts=50))
+    runner = MIMICRunner(fn, dict(keep_pct=0.2, pop_size=1000, max_attempts=50))
     
     curves = make_curve(suite, runner, dict(pop_size=[200, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000]), runs=runs, is_product=False)
     plt.figure()
