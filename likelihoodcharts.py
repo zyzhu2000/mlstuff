074ab49bf3ab9893cd68661671e0439fb36f2df8
Geoffrey Zhu <zyzhu2000@gmail.com>
2020-10-07 23:15:15 -0500

    running :-)

--- likelihoodcharts.py
+++ likelihoodcharts.py
@@ -73,7 +73,7 @@ def make_ga_mate():
     runs = 20
     suite = TestSuite(0)
     fn = CSequence()
-    runner = GARunner(fn, dict(max_attempts=8, pop_size=200,  mutation_prob=0.5, elite_dreg_ratio=0.99))
+    runner = GARunner(fn, dict(max_attempts=8, pop_size=200,  mutation_prob=0.1, elite_dreg_ratio=0.99))
     
     curves = make_curve(suite, runner, dict(pop_breed_percent=[0.5, 0.6, 0.75, 0.9]), runs=runs, is_product=False, extend=True)
     plt.figure()
